0150f6534b49c335093884e9e7a5aea19d9658e5
kristak313 <krista@alpinspire.com>
2015-06-07 16:11:12 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,16 +3,18 @@ allData <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.s
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
 
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+## Subsetting collected data as specified
+data <- subset(allData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(allData)
 
-## Converting dates
+## Convert dates as specified
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Creating plot number 2
+## Create plot 1
 plot(data$Global_active_power~data$Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
+
+## Copy plot to PNG file with specified height and width
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
